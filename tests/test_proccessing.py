6e88ab1273f91c0dfbf6ceddc002122ef9ba4623
Svyatoslav Deinekin <svyadey@mail.ru>
2023-11-06 04:43:46 +0300

    Fix naming list_dict -> list_dict_with_input_data

--- tests/test_proccessing.py
+++ tests/test_proccessing.py
@@ -4,7 +4,7 @@ from src.proccessing import get_list_by_key, sort_by_date
 
 
 @pytest.fixture
-def list_dict():
+def list_dicts_with_input_data():
     return [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
             {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
             {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
@@ -17,19 +17,19 @@ def list_dict():
                           ('CANCELED', [{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
                                         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]),
                           ])
-def test_get_list_by_key(list_dict, state, expected):
-    assert get_list_by_key(list_dict, key=state) == expected
+def test_get_list_by_key(list_dicts_with_input_data, state, expected):
+    assert get_list_by_key(list_dicts_with_input_data, key=state) == expected
 
 
 @pytest.mark.parametrize('by_raise, expected',
                          [(True, [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                                   {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
-                                   {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                                   {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]),
-                          (False, [{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-                                  {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
                                   {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
-                                  {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}]),
+                                  {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                                  {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]),
+                          (False, [{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                                   {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                                   {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+                                   {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}]),
                           ])
-def test_sort_by_date(list_dict, by_raise, expected):
-    assert sort_by_date(list_dict, by_raise=by_raise) == expected
+def test_sort_by_date(list_dicts_with_input_data, by_raise, expected):
+    assert sort_by_date(list_dicts_with_input_data, by_raise=by_raise) == expected
