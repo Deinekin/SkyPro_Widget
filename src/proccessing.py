94061a06c76395abeadaa8903fac5746060a3c53
Svyatoslav Deinekin <svyadey@mail.ru>
2023-11-26 22:49:28 +0300

    Hw_10 done

--- src/proccessing.py
+++ src/proccessing.py
@@ -1,3 +1,7 @@
+import re
+from collections import Counter
+
+
 def get_list_by_key(input_list: list[dict], key: str = "EXECUTED") -> list[dict]:
     """
     принимает на вход список словарей и значение для ключа state
@@ -23,3 +27,24 @@ def sort_by_date(input_list: list[dict], by_raise: bool = True) -> list[dict]:
     :return: отсортированный по дате список словарей
     """
     return sorted(input_list, key=lambda element: element['date'], reverse=by_raise)
+
+
+def search_by_description(transactions: list[dict], description: str) -> list[dict]:
+    """
+    Создает новый список словарей, которые в описании имеют заданную пользователем строку
+    :param transactions: список словарей с данными о банковских операциях
+    :param description: описание операции, по которой будет производиться поиск
+    :return: список словарей, у которых в описании есть заданная строка
+    """
+    return [transaction for transaction in transactions if
+            re.search(description.lower(), transaction['description'].lower())]
+
+
+def count_operations_by_category(transactions: list[dict], category_operations: dict) -> dict:
+    """
+
+    :param transactions: список словарей с данными о банковских операциях
+    :param category_operations:
+    :return: словарь, где ключи - описание операций, а значения 0 количество таких операций
+    """
+    return dict(Counter([transaction['description'] for transaction in transactions]))
