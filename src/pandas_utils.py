11044d4c4e4e056489e6882612ab29500ea65048
Svyatoslav Deinekin <svyadey@mail.ru>
2023-11-26 05:31:49 +0300

    hw_9 done?

--- src/pandas_utils.py
+++ src/pandas_utils.py
@@ -1,32 +1,26 @@
 import pandas as pd
 from typing import Any
+from pprint import pprint
 
 
-def set_dict(row_of_file: list, file: str) -> Any:
+def set_dict(row_of_file: list) -> Any:
     """
     Заполняет словарь элементами списка, состоящего из данных о транзакции
     :param row_of_file: строка из .csv или .xlsx файла с данными о транзакции
     :param file:
     :return: словарь, значениями которого являются элементы поданного на вход списка
     """
-    transaction_dict = {
-        "id": None,
+    return {
+        "id": row_of_file[0],
         "state": row_of_file[1],
         "date": str(row_of_file[2])[:-1],
-        "operationAmount": {"amount": None,
+        "operationAmount": {"amount": row_of_file[3],
                             "currency": {"currency_name": row_of_file[4],
                                          "currency_code": row_of_file[5]}},
         "from": row_of_file[6],
         "to": row_of_file[7],
         "description": row_of_file[8],
     }
-    if file.endswith('.csv'):
-        transaction_dict["id"] = row_of_file[0]
-        transaction_dict["operationAmount"]["amount"] = row_of_file[3]
-    else:
-        transaction_dict["id"] = str(row_of_file[0])[:-2]
-        transaction_dict["operationAmount"]["amount"] = str(row_of_file[3])[:-2]
-    return transaction_dict
 
 
 def read_csv_or_xlsx_file(file: str) -> list[dict]:
@@ -37,12 +31,12 @@ def read_csv_or_xlsx_file(file: str) -> list[dict]:
     """
     list_of_transactions_from_file: list = []
     if file.endswith('.csv'):
-        df = pd.read_csv(file, delimiter=';', encoding='utf-8')
+        df = pd.read_csv(file, delimiter=';', encoding='utf-8', dtype=str)
         for row in df.iterrows():
-            list_of_transactions_from_file.append(set_dict(row[1].to_list(), file))
+            list_of_transactions_from_file.append(set_dict(row[1].to_list()))
     if file.endswith('.xlsx'):
-        df = pd.read_excel(file)
+        df = pd.read_excel(file, dtype=str)
         for row in df.iterrows():
-            list_of_transactions_from_file.append(set_dict(row[1].to_list(), file))
+            list_of_transactions_from_file.append(set_dict(row[1].to_list()))
 
     return list_of_transactions_from_file
